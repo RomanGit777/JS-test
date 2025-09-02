a2c6a18b84c3688100027fd58d0f41e423873774
RomanGit777 <rsavenko91@gmail.com>
2025-09-02 13:31:53 +0200

    started to work with postD.js

--- test 2/scripts/postD.js
+++ test 2/scripts/postD.js
@@ -1,14 +1,7 @@
-// const container = document.getElementById('post-container');
-// const urlParams = new URLSearchParams(window.location.search);
-// const userId = urlParams.get("id");
-// fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`)
-// .then(res => res.json())
-// .then(data => {
-//     console.log(data);
-// })
-// const btn = document.createElement("button");
-// btn.innerText = 'Post Of Current User';
-// btn.addEventListener('click', () => {
-//     window.location.href = `post-details.html?id=${userId}`;
-// });
-// container.appendChild(btn);
+const container = document.getElementById('post-container');
+const urlParams = new URLSearchParams(window.location.search);
+const postId = urlParams.get("id");
+fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+.then(res => res.json())
+.then(data => {
+})
